04aafc81a13cf3a7061818d7917f96ba07ce541e
Denys Lapitan <d.lapitan@itransition.com>
2021-12-23 11:38:08 +0200

    fix Models

--- src/main/kotlin/com/example/petproject/model/BaseEntity.kt
+++ src/main/kotlin/com/example/petproject/model/BaseEntity.kt
@@ -2,14 +2,17 @@ package com.example.petproject.model
 
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 import java.util.UUID.randomUUID
+import javax.persistence.EntityListeners
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType.AUTO
 import javax.persistence.Id
 import javax.persistence.MappedSuperclass
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
 abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = AUTO)
--- src/main/kotlin/com/example/petproject/model/InsuranceKit.kt
+++ src/main/kotlin/com/example/petproject/model/InsuranceKit.kt
@@ -1,9 +1,7 @@
 package com.example.petproject.model
 
-import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 import javax.persistence.Entity
-import javax.persistence.EntityListeners
 import javax.persistence.EnumType.STRING
 import javax.persistence.Enumerated
 import javax.persistence.JoinColumn
@@ -13,7 +11,6 @@ import javax.persistence.Table
 
 @Entity
 @Table(name = "insurance_kit")
-@EntityListeners(AuditingEntityListener::class)
 class InsuranceKit: BaseEntity() {
     lateinit var duration: LocalDateTime
     var compensationPercent: Int = 0
