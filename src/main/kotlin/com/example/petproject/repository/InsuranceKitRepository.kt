1149d2dbd39caa01981fd37b3057bb91619c3da6
Denys Lapitan <d.lapitan@itransition.com>
2021-12-28 15:58:08 +0200

    fix restriction on the number of kits

--- src/main/kotlin/com/example/petproject/repository/InsuranceKitRepository.kt
+++ src/main/kotlin/com/example/petproject/repository/InsuranceKitRepository.kt
@@ -2,10 +2,14 @@ package com.example.petproject.repository
 
 import com.example.petproject.model.InsuranceKit
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface InsuranceKitRepository: JpaRepository<InsuranceKit, Long> {
 
     fun findByIdIn(id: List<Long>): Set<InsuranceKit>
+
+    @Query("select count(k) from InsuranceKit k")
+    fun countKits(): Long
 }
