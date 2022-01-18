92afd1af744f6be94036f65653824d23893ee294
Denis Lapitan <d.lapitan@itransition.com>
2022-01-18 18:36:56 +0200

    move @EnableJpaAuditing to config package

---
+++ src/main/kotlin/com/example/petproject/configuration/JpaAuditingConfiguration.kt
@@ -0,0 +1,8 @@
+package com.example.petproject.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {}
