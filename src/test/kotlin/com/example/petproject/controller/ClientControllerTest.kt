f098c8f21c2d8ba2947e7791bdf64ad5aff0db32
Denis Lapitan <d.lapitan@itransition.com>
2022-01-19 18:01:10 +0200

    added tests

--- src/test/kotlin/com/example/petproject/controller/ClientControllerTest.kt
+++ src/test/kotlin/com/example/petproject/controller/ClientControllerTest.kt
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultActions
 import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
@@ -37,12 +38,40 @@ internal class ClientControllerTest {
         firstName = "name",
         lastName = "name"
     )
-    private val invalidDto = ClientDto(
-        email = "",
-        password = "",
-        birthDate = LocalDate.of(1, 2, 22),
-        firstName = "",
-        lastName = ""
+    private val shortFirstName = ClientDto(
+        email = "2@fmail.com",
+        password = "123456",
+        birthDate = LocalDate.of(2000, 2, 22),
+        firstName = "name",
+        lastName = "n"
+    )
+    private val shortLastName = ClientDto(
+        email = "2@fmail.com",
+        password = "123456",
+        birthDate = LocalDate.of(2000, 2, 22),
+        firstName = "n",
+        lastName = "name"
+    )
+    private val birthDateFromTheFuture = ClientDto(
+        email = "2@fmail.com",
+        password = "123456",
+        birthDate = LocalDate.of(2222, 2, 22),
+        firstName = "name",
+        lastName = "name"
+    )
+    private val shortPassword = ClientDto(
+        email = "2@fmail.com",
+        password = "1",
+        birthDate = LocalDate.of(2000, 2, 22),
+        firstName = "name",
+        lastName = "n"
+    )
+    private val invalidEmail = ClientDto(
+        email = "2email",
+        password = "123456",
+        birthDate = LocalDate.of(2000, 2, 22),
+        firstName = "name",
+        lastName = "n"
     )
 
     @Test
@@ -59,31 +88,39 @@ internal class ClientControllerTest {
 
     @Test
     fun `add new valid client`() {
-        mockMvc.perform(
-            MockMvcRequestBuilders.post(path)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsBytes(validDto))
-        ).andExpect(status().isOk)
+        sendDto(validDto).andExpect(status().isOk)
     }
 
     @Test
-    fun `add new invalid client`() {
-        mockMvc.perform(
-            MockMvcRequestBuilders.post(path)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsBytes(invalidDto))
-        ).andExpect(status().is4xxClientError)
+    fun `add new client with short firstName`() {
+        sendDto(shortFirstName).andExpect(status().is4xxClientError)
     }
 
-    @ParameterizedTest
-    @EmptySource
-    fun `add new client with empty email`(email: String) {
-        mockMvc.post(path) {
-            param("email", email)
-            param("password", "123456")
-            param("birthDate", "2000-01-02")
-            param("firstName", "Dio")
-            param("lastName", "Maria")
-        }.andExpect { status().is4xxClientError }
+    @Test
+    fun `add new client with short lastName`() {
+        sendDto(shortLastName).andExpect(status().is4xxClientError)
+    }
+
+    @Test
+    fun `add new client from the future birthDate`() {
+        sendDto(birthDateFromTheFuture).andExpect(status().is4xxClientError)
+    }
+
+    @Test
+    fun `add new client with short password`() {
+        sendDto(shortPassword).andExpect(status().is4xxClientError)
+    }
+
+    @Test
+    fun `add new client with invalid email`() {
+        sendDto(invalidEmail).andExpect(status().is4xxClientError)
+    }
+
+    private fun sendDto(dto: ClientDto): ResultActions {
+        return mockMvc.perform(
+            MockMvcRequestBuilders.post(path)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsBytes(dto))
+        )
     }
 }
