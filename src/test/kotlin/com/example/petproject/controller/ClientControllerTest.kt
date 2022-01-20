b298add3e3923eaca0c83ceba29cd7c20a0257f3
Denis Lapitan <d.lapitan@itransition.com>
2022-01-20 16:24:24 +0200

    added tests

--- src/test/kotlin/com/example/petproject/controller/ClientControllerTest.kt
+++ src/test/kotlin/com/example/petproject/controller/ClientControllerTest.kt
@@ -36,13 +36,13 @@ internal class ClientControllerTest {
 
     @Test
     fun `get client`() {
-        mockMvc.get(Companion.PATH)
+        mockMvc.get(PATH)
             .andExpect { status().isOk }
     }
 
     @Test
     fun `find client`() {
-        mockMvc.get("${Companion.PATH}/e")
+        mockMvc.get("${PATH}/e")
             .andExpect { status().isOk }
     }
 
@@ -55,7 +55,7 @@ internal class ClientControllerTest {
 
     private fun MockMvc.post(dto: ClientDto): ResultActions =
         perform(
-            MockMvcRequestBuilders.post(Companion.PATH)
+            MockMvcRequestBuilders.post(PATH)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(mapper.writeValueAsBytes(dto))
         )
