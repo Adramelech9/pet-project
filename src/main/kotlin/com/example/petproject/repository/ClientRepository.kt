7eed8c5a99c71b9e50b27edcfe5a379488275069
Denis Lapitan <d.lapitan@itransition.com>
2022-01-20 17:04:10 +0200

    added tests

--- src/main/kotlin/com/example/petproject/repository/ClientRepository.kt
+++ src/main/kotlin/com/example/petproject/repository/ClientRepository.kt
@@ -2,8 +2,6 @@ package com.example.petproject.repository
 
 import com.example.petproject.model.Client
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 
-@Repository
 interface ClientRepository : JpaRepository<Client, Long>, ClientRepositoryCustom {
 }
